810b1cf194b39738e7c1a9212701d0f419e5376d
John Ott <john@johnott.us>
2015-10-30 17:03:59 -0400

    Raw image

--- lib/launch.sh
+++ lib/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/qemu/x86_64-softmmu
-./qemu-system-x86_64 -cdrom ~/archlinux-2015.10.01-dual-patched.iso -vnc :0 -monitor stdio -m 1G -smp 2
+./qemu-system-x86_64 -cdrom ~/archlinux-2015.10.01-dual-patched.iso -vnc :0 -monitor stdio -m 1G -smp 2 -drive file=~/Arch.img,index=0,media=disk,format=raw
