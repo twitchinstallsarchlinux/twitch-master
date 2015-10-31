c21c262ac4f8423b2edda52302e48f09b0e0d933
John Ott <john@johnott.us>
2015-10-30 21:23:24 -0400

    Add map testing file

---
+++ test_map.js
@@ -0,0 +1,37 @@
+var fs  = require('fs');
+var pub = require('./lib/comm').sender();
+
+
+// Load json command mapping
+var map = {}
+
+function map_load() {
+  fs.exists('map.json', function() {
+    try {
+      var map_new = JSON.parse(fs.readFileSync('map.json', 'utf8'));
+      map = map_new;
+      console.log('(Re)loaded map.json');
+    } catch (ex) {
+      console.log('Could not load map.json');
+      console.log(ex);
+    }
+  });
+}
+
+map_load();
+
+var length = 0;
+
+function iter() {
+  var next = Object.keys(map)[length];
+  if (next != null) {
+    console.log('Sending: ' + next + ' -> ' + map[next]);
+    pub.send(['qemu-manager', map[next]]);
+    length++;
+    setTimeout(iter, 2 * 1000);
+  } else {
+    console.log('Done!');
+  }
+}
+
+setTimeout(iter, 2 * 1000);
