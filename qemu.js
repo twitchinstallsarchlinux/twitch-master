24ac65a88c7eb49ec1e08bd47234740033be516c
David Lee <dlow.mail@gmail.com>
2015-11-01 14:15:06 -0500

    Update qemu.js
    Respawn qemu process on exit

--- qemu.js
+++ qemu.js
@@ -1,17 +1,36 @@
 var sub = require('./lib/comm').receiver('qemu-manager');
 console.log('Connecting to master...');
 sub.monitor();
-var spawn = require('child_process').spawn;
 
-var child = spawn('bash', ['lib/launch.sh']);
+var child = null;
+function spawn_process(child_process) {
+  child_process = spawn('bash', ['lib/launch.sh']);
 
-child.stdout.on('data', function(data) {
-  console.log('QEMU: ' + data);
-});
+  child_process.stdout.on('data', function(data) {
+    console.log('QEMU: ' + data);
+  });
 
-child.stderr.on('data', function(data) {
-  console.log('ERR QEMU: ' + data);
-});
+  child_process.stderr.on('data', function(data) {
+    console.log('ERR QEMU: ' + data);
+  });
+
+  // respawn process on error? Not sure if this will trigger
+  // an exit event, spawning two processes
+  //child_process.on('error', function(data) {
+  //  console.log('PROCERR QEMU: ' + data);
+  //  child_process.kill('SIGKILL');
+  //  spawn_process(child);
+  //});
+
+  //respawn process if it exits
+  child_process.on('exit', function(data) {
+    console.log('EXIT QEMU: ' + data);
+    spawn_process(child);
+  });
+
+  child = child_process;
+}
+spawn_process(child);
 
 sub.on('connect', function() {
   console.log('Connected to master');
